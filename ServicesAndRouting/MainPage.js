621102de78abc9f2d1dce3482238eb06e366a9da
ltfuser01 <ltfuser01@AZID1WLTF01>
2016-12-23 12:28:46 +0000

    changes

--- ServicesAndRouting/MainPage.js
+++ ServicesAndRouting/MainPage.js
@@ -2,16 +2,43 @@
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-        .when('/about',   { templateUrl: 'views/About.html' })
+        .when('/about', { templateUrl: 'views/About.html' })
         .when('/contact', { templateUrl: 'views/Contact.html' })
-        .when('/welcome', { templateUrl: 'views/Welcome.html' })
+        .when('/welcome',
+
+                {
+                    templateUrl: 'views/Welcome.html',
+                    controller: 'ProductCtrl',
+                    controllerAs: 'vm',
+                    prodDetails: { name: 'Envision', domain: 'Tech Vision' }
+                })
+        .when('/product/:productid',
+                {
+                    templateUrl: 'views/Product.html',
+                    controller: 'ProductCtrl',
+                    controllerAs: 'vm',
+                    prodDetails: { name: 'Fleetcor', domain: 'Fuel Cart' }
+                })
+    .otherwise({ redirectTo: '/about' })
 }])
 
-app.controller('MainCtrl', function ($scope) {
-    
+app.controller('NavCtrl', function ($location) {
+
+    this.goto = function (path) {
+        $location.replace();
+        $location.path(path);
+    };
 });
 
+app.controller('ProductCtrl', ['$route', function ($route) {
+    var vm = this;
+    vm.details = "Hello, from Product controller";
+    vm.prodDetails = $route.current.prodDetails;
+   
 
-app.controller('ProductCtrl', function ($scope) {
+}]);
 
+app.controller('WelcomeCtrl', function ($scope) {
+    this.details = "Hello, from Welcome controller";
+    vm.prodDetails = $route.current.prodDetails;
 });
